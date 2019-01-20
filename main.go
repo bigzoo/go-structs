65fa9ca8ebdf36b789c4992204863175a85dc51a
Chris Nyaga <nyagachris75@gmail.com>
2019-01-20 12:05:17 +0300

    more struction

--- main.go
+++ main.go
@@ -9,6 +9,9 @@ type person struct {
 
 func main() {
 	alex := person{"Alex", "Anderson"} // one way of creating a person. values are assigned according to order
+	// var alex person // creates a var of type person. the fields are assigned zero values ie empty strings here
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
 	alex = person{firstName: "Alex", lastName: "Anderson"} // better way of creating var's
 	fmt.Println(alex)
 }
