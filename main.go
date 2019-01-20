43739849457af2c1f06051d0e86f9b9da9ddd853
Chris Nyaga <nyagachris75@gmail.com>
2019-01-20 12:11:08 +0300

    structs in structs

--- main.go
+++ main.go
@@ -2,16 +2,25 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main() {
-	alex := person{"Alex", "Anderson"} // one way of creating a person. values are assigned according to order
-	// var alex person // creates a var of type person. the fields are assigned zero values ie empty strings here
-	// alex.firstName = "Alex"
-	// alex.lastName = "Anderson"
-	alex = person{firstName: "Alex", lastName: "Anderson"} // better way of creating var's
-	fmt.Println(alex)
+	jim := person{
+		firstName: "Jim",
+		lastName: "Party",
+		contact: contactInfo{
+			email: "jim@gmail.com",
+			zipCode: 94993,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
